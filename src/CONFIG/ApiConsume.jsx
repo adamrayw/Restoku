ea6e0aa479349bfb96e05d6ca175cb71f7a1cc29
HafizhMA <muhammadazhr29@gmail.com>
2023-08-19 14:39:02 +0700

    modifikasi code bagian apiConsume

--- src/CONFIG/ApiConsume.jsx
+++ src/CONFIG/ApiConsume.jsx
@@ -1,38 +1,14 @@
-// const apiRestaurantList = `https://restaurant-api.dicoding.dev/list`;
-// const apiRestaurantDetail = `https://restaurant-api.dicoding.dev/detail/`;
-
-// async function getListRestaurants() {
-//   const response = await fetch(apiRestaurantList);
-//   const data = await response.json();
-//   return data.restaurants;
-// }
-
-// async function getDetailRestaurant(id) {
-//   const response = await fetch(`${apiRestaurantDetail}${id}`);
-//   const data = await response.json();
-//   return data.restaurant;
-// }
-
-// export { getListRestaurants, getDetailRestaurant };
-
-// api.js
 const baseUrl = "https://restaurant-api.dicoding.dev";
 
-async function fetchData(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
 async function getListRestaurants() {
-  const apiRestaurantList = `${baseUrl}/list`;
-  const data = await fetchData(apiRestaurantList);
+  const response = await fetch(`${baseUrl}/list`);
+  const data = await response.json();
   return data.restaurants;
 }
 
 async function getDetailRestaurant(id) {
-  const apiRestaurantDetail = `${baseUrl}/detail/${id}`;
-  const data = await fetchData(apiRestaurantDetail);
+  const response = await fetch(`${baseUrl}/detail/${id}`);
+  const data = await response.json();
   return data.restaurant;
 }
 
